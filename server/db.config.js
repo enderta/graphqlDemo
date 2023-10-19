a38d7a045b28499f3dcc7a73cefd2e01d32f9e15
enderta <endertanver@gmail.com>
2023-10-19 13:09:57 +0100

    grapql demo

--- server/db.config.js
+++ server/db.config.js
@@ -4,11 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-    /* user: process.env.DB_USER || "postgres",
-    host: process.env.DB_HOST || "localhost",
-    database: process.env.DB_DATABASE || "jobs",
-    password: process.env.DB_PASSWORD || "ender",
-    port: process.env.DB_PORT || 5432, */
+   
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
