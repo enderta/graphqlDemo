fe91dbb051e27de4b79778e83c001ee87f5a497f
enderta <endertanver@gmail.com>
2023-10-19 13:07:31 +0100

    grapql demo

--- src/components/ApolloProvider.js
+++ src/components/ApolloProvider.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', 
+  uri: process.env.REACT_APP_URI,
   cache: new InMemoryCache(),
 });
+
 const ApolloAppProvider = ({ children }) => {
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
+
 export default ApolloAppProvider;
